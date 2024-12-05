b14bec01b56e4f0741cdc3ee9a65f853b565d92d
Keisuke Furuya <1180samuraijp@gmail.com>
2024-12-05 18:23:12 +0900

    ファイルを分割した

---
+++ 9cc.h
@@ -0,0 +1,67 @@
+#ifndef NINECC_H
+#define NINECC_H
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef enum {
+  TK_RESERVED, // Keywords or punctuators
+  TK_NUM,      // Integer literals
+  TK_EOF,      // End-of-file markers
+} TokenKind;
+
+// Token type
+typedef struct Token Token;
+struct Token {
+  TokenKind kind; // Token kind
+  Token *next;    // Next token
+  int val;        // If kind is TK_NUM, its value
+  char *str;      // Token string
+  int len;        // Token length
+};
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+  NodeKind kind; // Node kind
+  Node *lhs;     // Left-hand side
+  Node *rhs;     // Right-hand side
+  int val;       // Used if kind == ND_NUM
+};
+
+// Input program
+extern char *user_input;
+
+// Current token
+extern Token *token;
+
+// Tokenaize
+
+void error(char *fmt, ...);
+Token *tokenize();
+Node *expr();
+
+// codegen
+
+void gen(Node *node);
+
+#endif // NINECC_H
+
+
--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
-CFLAGS=-std=c11 -g -static
+CFLAGS=-std=c11 -g -static -Wa,--noexecstack
+SRCS=$(wildcard *.c)
+OBJS=$(SRCS:.c=.o)
 
-chibicc: main.o
-	$(CC) -o $@ $? $(LDFLAGS)
+9cc: $(OBJS)
+	$(CC) -o 9cc $(OBJS) $(LDFLAGS)
 
-test: chibicc
+$(OBJS): 9cc.h
+
+test: 9cc
 	./test.sh
 
 clean:
-	rm -f chibicc *.o *~ tmp*
+	rm -f 9cc *.o *~ tmp*
 
 .PHONY: test clean
